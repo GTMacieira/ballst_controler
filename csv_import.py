146974a74e725eeda94347d556bc826af7ac5c2d
Guilherme Macieira <macieira.gtalpo@gmail.com>
2022-09-21 14:43:05 -0300

    Subir csv

--- csv_import.py
+++ csv_import.py
@@ -12,7 +12,7 @@ cur = con.cursor()
 # 	fatura VARCHAR(100) NOT NULL,
 # 	moeda VARCHAR(5) NOT NULL,
 # 	val_fatura FLOAT NOT NULL,
-# 	utilizacao_fatura REAL NOT NULL,
+# 	utilizacao_fatura FLOAT NOT NULL,
 # 	val_utilizado_fatura FLOAT NOT NULL,
 # 	val_util_convert_brl FLOAT NOT NULL,
 # 	val_util_convert_eur FLOAT NOT NULL,
@@ -29,11 +29,12 @@ cur = con.cursor()
 # )
 # ;""")
 
+
 with open('C:/Users/CSO5007/Desktop/python_projects/ballst_controler/recursos/DB_Lastros_CSV.csv','r') as fin: # `with` statement available in 2.5+
     # csv.DictReader uses first line in file for column headings by default
     dr = csv.DictReader(fin) # comma is default delimiter
-    to_db = [(['sequencial'], ['novo_sequencial'],['cliente'], ['dt_embarque'],['dt_recebimento'], ['fatura'], ['moeda'], ['val_fatura'],['utilizacao_fatura'], ['val_utilizado_fatura'], ['val_util_convert_brl'], ['val_util_convert_eur'], ['val_util_convert_usd'], ['tp_contrato'], ['sd_due'], ['ch_acesso_re'], ['obs'], ['total_parcial'], ['ultimo_ano'], ['user'], ['register']) for i in dr]
+    to_db = [(i['sequencial'], i['novo_sequencial'],i['cliente'], i['dt_embarque'],i['dt_recebimento'], i['fatura'], i['moeda'], i['valfatura'],i['utilizacao_fatura'], i['val_utilizado_fatura'], i['val_util_convert_brl'], i['val_util_convert_eur'], i['val_util_convert_usd'], i['tp_contrato'], i['sd_due'], i['ch_acesso_re'], i['obs'], i['total_parcial'], i['ultimo_ano'], i['user'], i['register']) for i in dr]
+    cur.executemany("INSERT INTO ballests (sequencial, novo_sequencial, cliente, dt_embarque, dt_recebimento, fatura, moeda, val_fatura, utilizacao_fatura, val_utilizado_fatura, val_util_convert_brl, val_util_convert_eur, val_util_convert_usd, tp_contrato, sd_due, ch_acesso_re, obs, total_parcial, ultimo_ano, user, register) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);", to_db)
+    con.commit()
+    con.close()
 
-cur.executemany('insert into ballests (sequencial, novo_sequencial, cliente, dt_embarque, dt_recebimento, fatura, moeda, val_fatura, utilizacao_fatura, val_utilizado_fatura, val_util_convert_brl, val_util_convert_eur, val_util_convert_usd, tp_contrato, sd_due, ch_acesso_re, obs, total_parcial, ultimo_ano, user, register) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);', to_db)
-con.commit()
-con.close()
