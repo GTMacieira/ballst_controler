8fb5e824f55f45ed649609c3efde6d9f2bfd050f
Guilherme Macieira <macieira.gtalpo@gmail.com>
2022-09-22 17:36:16 -0300

    Ajuste CSV

--- csv_import.py
+++ csv_import.py
@@ -2,31 +2,31 @@ import csv, sqlite3
 
 con = sqlite3.connect("ball_control.db") # change to 'sqlite:///your_filename.db'
 cur = con.cursor()
-cur.execute("""CREATE TABLE ballests (
-	id INTEGER NOT NULL,
-	sequencial VARCHAR(20) NOT NULL,
-	novo_sequencial VARCHAR(20) NULL,
-	cliente VARCHAR(100) NOT NULL,
-	dt_embarque DATE NOT NULL,
-	dt_recebimento DATE NOT NULL,
-	fatura VARCHAR(100) NOT NULL,
-	moeda VARCHAR(5) NOT NULL,
-	val_fatura FLOAT NOT NULL,
-	utilizacao_fatura FLOAT NOT NULL,
-	val_utilizado_fatura FLOAT NOT NULL,
-	val_util_convert_brl FLOAT NOT NULL,
-	val_util_convert_eur FLOAT NOT NULL,
-	val_util_convert_usd FLOAT NOT NULL,
-	tp_contrato VARCHAR(10) NOT NULL,
-	sd_due VARCHAR(500) NOT NULL,
-	ch_acesso_re VARCHAR(500) NULL,
-	obs VARCHAR(500) NULL,
-	total_parcial VARCHAR(10) NOT NULL,
-	user VARCHAR(20) NOT NULL,
-	register DATETIME NOT NULL,
-	PRIMARY KEY ("id")
-)
-;""")
+# cur.execute("""CREATE TABLE ballests (
+# 	id INTEGER NOT NULL,
+# 	sequencial VARCHAR(20) NOT NULL,
+# 	novo_sequencial VARCHAR(20) NULL,
+# 	cliente VARCHAR(100) NOT NULL,
+# 	dt_embarque DATE NOT NULL,
+# 	dt_recebimento DATE NOT NULL,
+# 	fatura VARCHAR(100) NOT NULL,
+# 	moeda VARCHAR(5) NOT NULL,
+# 	val_fatura FLOAT NOT NULL,
+# 	utilizacao_fatura FLOAT NOT NULL,
+# 	val_utilizado_fatura FLOAT NOT NULL,
+# 	val_util_convert_brl FLOAT NOT NULL,
+# 	val_util_convert_eur FLOAT NOT NULL,
+# 	val_util_convert_usd FLOAT NOT NULL,
+# 	tp_contrato VARCHAR(10) NOT NULL,
+# 	sd_due VARCHAR(500) NOT NULL,
+# 	ch_acesso_re VARCHAR(500) NULL,
+# 	obs VARCHAR(500) NULL,
+# 	total_parcial VARCHAR(10) NOT NULL,
+# 	user VARCHAR(20) NOT NULL,
+# 	register DATETIME NOT NULL,
+# 	PRIMARY KEY ("id")
+# )
+# ;""")
 
 
 with open('C:/Users/CSO5007/Desktop/python_projects/ballst_controler/recursos/DB_Lastros_CSV.csv','r') as fin: # `with` statement available in 2.5+
