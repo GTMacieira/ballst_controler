8319677ad6c36f9b8b7d1d57db0319cf8a38571d
Guilherme Macieira <macieira.gtalpo@gmail.com>
2022-09-29 15:38:26 -0300

    Criação do menu

--- ballst_control.py
+++ ballst_control.py
@@ -2,6 +2,25 @@
 import getpass
 import time
 from db_operator import *
+
+
+menu = input("1- Novo sequencial\n\
+    2- Novo tipo de divida\n\
+    3- Novo lastro\n\
+    4- Repactuação\n\
+    5- Atribuir Lastro excedente\n\
+    6- Relatório mesa\n")
+    
+dts_debt = ["seq","new_seq","tp_cont","dt_cont","moeda","val_princ","inst_financ","dt_ini","dt_fim","emp","tp_div","class","cont"]
+
+
+if menu == "1":
+    for dt in dts_debt:
+        print(dt)
+        dt = input(f"{dt} ")
+        print(f"{dt}")
+    
+    
     
 # Cria coneão com db
 connection = con_creation()
